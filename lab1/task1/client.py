47a1b2e61eabb00e624c066615ab4e07f902477e
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-10-07 22:14:04 +0300

    Lab1 Task1.2 TCP-server, TCP-client

--- lab1/task1/client.py
+++ lab1/task1/client.py
@@ -3,8 +3,10 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(("127.0.0.1", 5689))
 
-print("Send something to the server\n")
-print("> ", end="")
+messageToServer = "This is a test message with 44 bytes of data"
+print(f"\n> {messageToServer}")
 
-messageToServer = input()
 socket.send(messageToServer.encode())
+
+messageFromServer = socket.recv(1024).decode()
+print(f"\nReceived from server: {messageFromServer}\n")
--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -1,13 +1,20 @@
 import socket
-from time import gmtime, strftime
+from time import gmtime, strftime, sleep
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(("127.0.0.1", 5689))
 socket.listen()
 
 conn, addr = socket.accept()
+print(f"\nConnected by {addr}\n")
 
-print(f"Message from client:\n")
+messageFromClient = conn.recv(1024)
+sleep(5)
 
-messageFromClient = conn.recv(1024).decode()
-print(f"{strftime('%H:%M:%S')}  {messageFromClient}")
+if len(messageFromClient) == 44:
+    respons = 'All data received successfully'
+else:
+    respons = 'Data transmission error'
+
+conn.send(respons.encode())
+conn.close()
