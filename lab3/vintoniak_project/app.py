7663a025e529f744977768c50956562bd5edba44
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-10-16 22:31:24 +0300

    Lab3 Created Home page

--- lab3/vintoniak_project/app.py
+++ lab3/vintoniak_project/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+from data import certificats
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', certificats=certificats)
 
 if __name__ == '__main__':  
     app.run(debug=True)
---
+++ lab3/vintoniak_project/data.py
@@ -0,0 +1,16 @@
+certificats = [
+    {
+        "name" : "CPA: Programming Essentials in C++",
+        "platform": "Cisco",
+        "instractorName": "Mykola Kozlenko",
+        "date": "3 Jun 2022",
+        "photoName": "C++"
+    },
+    {
+        "name" : "Python and Flask Bootcamp",
+        "platform": "Udemy",
+        "instractorName": "Jose Portilla",
+        "date": "29 Aug 2023",
+        "photoName": "pythonFlak"
+    }
+]
