936614ccb8b563c2992fa37fcdf61cb06d9387cc
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-10-30 14:10:17 +0200

    Lab6 Create an empty database

--- lab3/vintoniak_project/app/__init__.py
+++ lab3/vintoniak_project/app/__init__.py
@@ -1,5 +1,14 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from app import views
