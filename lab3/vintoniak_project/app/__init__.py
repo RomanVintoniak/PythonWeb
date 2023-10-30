2ebf2c33850a8fba41c8008069a885d277e569a5
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-10-30 22:43:41 +0200

    Lab6 Add new column using Flask-Migrate

--- lab3/vintoniak_project/app/__init__.py
+++ lab3/vintoniak_project/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,5 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 from app import views
