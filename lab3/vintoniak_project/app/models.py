fece4c002586921c4f029d70ea766bda7f273f52
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-11-04 15:29:19 +0200

    Lab7 Task4 Add users table to DB using migrate

--- lab3/vintoniak_project/app/models.py
+++ lab3/vintoniak_project/app/models.py
@@ -29,7 +29,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Colimn(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image = db.Column(db.String(20),  nullable=False, default="default.jpg")
     password = db.Column(db.String(120), nullable=False)
     
