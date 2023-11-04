01eaa7840049792026e76fb18f970481bb045f45
Roman Vintoniak <romanvintoniak03@gmail.com>
2023-11-04 16:15:19 +0200

    Lab7 Task5 Add Regisration form, update Login form

--- lab3/vintoniak_project/app/form.py
+++ lab3/vintoniak_project/app/form.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField, EmailField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired("This field is required")])
+    email = StringField("Email", validators=[DataRequired("This field is required"), Email("Please enter your email address")])
     password = PasswordField("Password", validators=[DataRequired("This field is required"), Length(min=4, max=10)])
     rememberMe = BooleanField("Remember me")
     submit = SubmitField("Log in")
+    
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired("This field is required"), Length(min=4, max=10)])
+    email = EmailField("Email", validators=[DataRequired("This field is required"), Email("Please enter your email address")])
+    password = PasswordField("Password", validators=[DataRequired("This field is required"), Length(min=6)])
+    confirmPassword = PasswordField("Confirm password", validators=[DataRequired("This field is required"), EqualTo("password", "Passwords do not match")])
+    submit = SubmitField("Sign up")
 
 class ChangePasswordForm(FlaskForm):
     password = PasswordField("Enter new password", validators=[DataRequired("This field is required"), Length(min=4, max=10)])
